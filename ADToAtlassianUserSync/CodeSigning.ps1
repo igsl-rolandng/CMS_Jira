b9b628d88b73c58cf20c2c3c7d59c913dc27583f
kcwongigs <106725013+kcwongigs@users.noreply.github.com>
2022-08-12 13:10:10 +0800

    Delete ADToAtlassianUserSync directory
    Selected wrong repository

--- ADToAtlassianUserSync/CodeSigning.ps1
+++
@@ -1,7 +0,0 @@
-$codeSignerDN = 'ADUserSync Code Signer'
-$codeSignerFile = 'ADUserSync Code Signer.cer'
-
-# Check if code signer cert exists
-$signerCert = Get-ChildItem -Path "Cert:\CurrentUser\My" | Where-Object { $_.Subject -match $codeSignerDN }
-Set-AuthenticodeSignature -File ADUserSync.ps1 -Cert $signerCert
-$signerCert | Export-Certificate -FilePath $codeSignerFile
